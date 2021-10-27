47c4a9f3152f9bb3a20080d35e49cbab430358a3
Craig Buchanan <craig.buchanan@cogniza.com>
2021-10-27 08:12:34 -0500

    adding help text
    adding support for zero-length strings and 'white space'

--- PsCoachManager/Private/nz.ps1
+++ PsCoachManager/Private/nz.ps1
@@ -1,3 +1,23 @@
+<#
+.SYNPOSIS
+Converts a [System.DBNull], a zero-length string, or 'white space' to a $null.  All other values returned unaltered.
+
+.PARAMETER InputObject
+The value to be tested and (potentially) converted.
+
+.EXAMPLE
+PS> [System.DBNull]::Value | nz
+$null
+
+.EXAMPLE
+PS> '' | nz
+$null
+
+.EXAMPLE
+PS> 'lorem ipsum' | nz
+lorem ipsum
+
+#>
 function nz
 {
     param(
@@ -5,5 +25,12 @@ function nz
         [object]$InputObject
     )
 
-    [System.DBNull]::Value.Equals($InputObject) ? $null : $InputObject
+    switch ( $InputObject ) {
+        { [System.DBNull]::Value.Equals($InputObject) } { $null }
+        { $_ -is [string] -and [string]::IsNullOrWhiteSpace($_) } { $null }
+        default { $InputObject }
+    }
+
+    # [System.DBNull]::Value.Equals($InputObject) ? $null : $InputObject
+
 }
